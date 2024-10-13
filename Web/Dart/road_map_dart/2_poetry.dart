975ec4e0da1947273083fb1ae453474039a2058a
strujilloespinosa <strujilloespinosa@gmail.com>
2024-10-13 02:41:27 +0000

    Operadores

---
+++ Web/Dart/road_map_dart/2_poetry.dart
@@ -0,0 +1,41 @@
+/*
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */
+//! DESAROOLLO.
+/** 
+ * Antes... Ten en cuenta lo siguiente...
+ * En dart al igual que en otros lenguajes, hay precedencia de operadores, es como una jerarquia de estos,
+ * esto significa que tendran mayor relevancia unos sobre otros, por ende se va a ejecutar de primeras a acomparacion 
+ * de los otros. 
+ * Ejemplo de precedencia de mayor a menor:]
+ * * https://dart.dev/language/operators#operator-precedence-example.
+*/
+//todo: NOTA: Cuando usamos operadores creamos, expresiones dentro del lenguaje.
+void main () {
+{
+  print("Suma --> + ${2 + 3}");
+  print("Resta --> - ${66 - 13}");
+  print("Negacion --> -expr -${10}");
+  print("Multiplicar --> *");
+  print("Dividir --> /");
+  print("Dividir devolviendo un entero --> ~/");
+  print("Obtenga el resto de una división entera (módulo) --> %");
+
+
+    
+}
+}
