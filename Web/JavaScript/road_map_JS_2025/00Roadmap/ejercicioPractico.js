980481669a5959eb0a815ed7a4c62d8f9aa5a32e
Poetry0354 <strujilloespinosa@gmail.com>
2025-06-06 22:15:53 -0500

    Ejercicio para 00 Roadmap

---
+++ Web/JavaScript/road_map_JS_2025/00Roadmap/ejercicioPractico.js
@@ -0,0 +1,9 @@
+/*
+ * Crear una estructura de datos para un usuario, manipularla
+ * y mostrar información basada en sus propiedades,
+ * prestando especial atención a los tipos de datos y cómo interactúan.
+ * */
+
+// Desarrollo de ejercicio práctico
+
+//Posd: Implementa bases de logica y algoritma...
